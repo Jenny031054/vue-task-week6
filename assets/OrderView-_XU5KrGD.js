1f318cd7b8db217245fa928d45237c0ffcdb8687
林炘秀 <jenny031054@gmail.com>
2024-02-23 11:07:23 +0800

    Updates

--- assets/OrderView-_XU5KrGD.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as o}from"./index-RE74IEgu.js";const r={},s={class:"fw-bold"};function t(n,_){return c(),o("h1",s,"這裡是訂單列表")}const f=e(r,[["render",t]]);export{f as default};
---
+++ assets/OrderView-mvY0Avas.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./index-u7S5IKh1.js";const r={},s={class:"fw-bold"};function t(n,_){return c(),o("h1",s,"這裡是訂單列表")}const f=e(r,[["render",t]]);export{f as default};
